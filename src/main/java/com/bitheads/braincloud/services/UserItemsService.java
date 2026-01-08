2df9bacf5112394cd778e6a108cba72c901e37e7
bitheadCody <113926498+bitheadCody@users.noreply.github.com>
2026-01-08 10:16:05 -0500

    Update signature (#63)
    * implemented new User Items APIs
    
    * added options param
    
    * added null check for optional parameter

--- src/main/java/com/bitheads/braincloud/services/UserItemsService.java
+++ src/main/java/com/bitheads/braincloud/services/UserItemsService.java
@@ -95,7 +95,9 @@ public class UserItemsService {
 			data.put(Parameter.defId.name(), defId);
 			data.put(Parameter.quantity.name(), quantity);
 			data.put(Parameter.includeDef.name(), includeDef);
-			data.put(Parameter.optionsJson.name(), new JSONObject(optionsJson));
+			if(optionsJson != null){
+				data.put(Parameter.optionsJson.name(), new JSONObject(optionsJson));
+			}
 
 			ServerCall sc = new ServerCall(ServiceName.userItems, ServiceOperation.AWARD_USER_ITEM, data, callback);
 			_client.sendRequest(sc);
@@ -182,16 +184,40 @@ public class UserItemsService {
 	 *                                the response.
 	 * @param includePromotionDetails If true, the promotion details of the eligible
 	 *                                promotions will be included in the response.
+	 * @param optionsJson             Optional support for specifying
+	 *                                'blockIfExceedItemMaxStackable' indicating how
+	 *                                to process
+	 *                                the award if the defId is for a stackable item
+	 *                                with a max
+	 *                                stackable quantity and the specified quantity
+	 *                                to award is
+	 *                                too high. If true and the quantity is too
+	 *                                high, the call
+	 *                                is blocked and an error is returned. If false
+	 *                                (default)
+	 *                                and quantity is too high, the quantity is
+	 *                                adjusted to the
+	 *                                allowed maximum and the quantity not awarded
+	 *                                is reported
+	 *                                in response key 'itemsNotAwarded' - unless the
+	 *                                adjusted
+	 *                                quantity would be 0, in which case the call is
+	 *                                blocked and
+	 *                                an error is returned.
 	 * @param callback                The method to be invoked when the server
 	 *                                response is received.
 	 */
 	public void getItemsOnPromotion(String shopId, boolean includeDef, boolean includePromotionDetails,
+			String optionsJson,
 			IServerCallback callback) {
 		try {
 			JSONObject data = new JSONObject();
 			data.put(Parameter.shopId.name(), shopId);
 			data.put(Parameter.includeDef.name(), includeDef);
 			data.put(Parameter.includePromotionDetails.name(), includePromotionDetails);
+			if(optionsJson != null){
+				data.put(Parameter.optionsJson.name(), new JSONObject(optionsJson));
+			}
 
 			ServerCall sc = new ServerCall(ServiceName.userItems, ServiceOperation.GET_ITEMS_ON_PROMOTION, data,
 					callback);
@@ -379,7 +405,9 @@ public class UserItemsService {
 			data.put(Parameter.quantity.name(), quantity);
 			data.put(Parameter.shopId.name(), shopId);
 			data.put(Parameter.includeDef.name(), includeDef);
-			data.put(Parameter.optionsJson.name(), new JSONObject(optionsJson));
+			if(optionsJson != null){
+				data.put(Parameter.optionsJson.name(), new JSONObject(optionsJson));
+			}
 
 			ServerCall sc = new ServerCall(ServiceName.userItems, ServiceOperation.PURCHASE_USER_ITEM, data, callback);
 			_client.sendRequest(sc);
